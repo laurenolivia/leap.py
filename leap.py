04118b95d009bee6568f80022b17c3350b68f73f
Lauren <lolivia.b@gmail.com>
2020-11-18 20:26:57 -0400

    removed comments

--- leap.py
+++ leap.py
@@ -5,10 +5,10 @@ def leap_challenge(n):
         return -1
     if n == 0 or n == 1:
         return n
-                                        #begins by summing 0, 1    
-    prev = 0                            #applying concept of singly linked list with prev == head
+                                          
+    prev = 0                            
     current = 1
-    for i in range(n-1):                #iterate through n-1 to return what last value would be, rather than executing calculation on the last value
+    for i in range(n-1):                
         next_num = (prev + current)     
         prev = current                  
         current = next_num
